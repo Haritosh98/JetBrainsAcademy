527509d0f43cfcc7aff8f14295c9b53a0e6d1088
Haritosh Modha <91212635+Haritosh98@users.noreply.github.com>
2022-02-24 11:28:26 +0000

    Update zookeeper_project.py

--- Python Core/Zookeeper/zookeeper_project.py
+++ Python Core/Zookeeper/zookeeper_project.py
@@ -122,8 +122,6 @@ It looks like we will soon have more rabbits!"""
 
 animals = [camel, lion, deer, goose, bat, rabbit]
 
-# write your code here
-
 while True:
     user = input('Please enter the number of the habitat you would like to view:')
     habitats = ['0', '1', '2' , '3', '4', '5']
